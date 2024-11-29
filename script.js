cc6094124115de1788a21c1ec8005742b89a14ec
Shamsiddin Abdurahmonov <sh.abdurahmonv@gmail.com>
2024-11-29 16:06:30 +0500

    portfolio-task

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Находим все кнопки с классом "header__link"
 const buttons = document.querySelectorAll(".header__link");
 
-// Добавляем обработчик клика на каждую кнопку
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    // Убираем активный класс со всех кнопок
     buttons.forEach((btn) => btn.classList.remove("active"));
 
-    // Добавляем активный класс на нажатую кнопку
     button.classList.add("active");
   });
 });
+// dark and light mode
 document.querySelector(".theme-btn").addEventListener("click", () => {
   document.body.classList.toggle("light-mode");
 });
